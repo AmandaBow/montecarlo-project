bc19af5226ac029466ed4affbf3d4cdb36f08ca2
Amanda Bowen <ab4798469@gmail.com>
2026-05-08 03:55:56 +0000

    add code from test to main

--- main.py
+++ main.py
@@ -1,1 +1,67 @@
 
+# import libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import random 
+
+# set seed to make code reproducable
+random.seed(42)
+
+def random_walk(grid_size, N, steps, n_trails):
+    # all particles start center
+    x = [grid_size_x // 2] * N
+    y = [grid_size_y // 2] * N
+
+    # heat accumulation grid
+    heat = np.zeros((grid_size_y, grid_size_x))
+
+    # store trajectories for a few particles
+    traj_x = [[center_x] for _ in range(n_trails)]
+    traj_y = [[center_y] for _ in range(n_trails)]
+
+    # particle movement
+    for step in range(steps):
+
+        # move every particle
+        for i in range(N):
+
+            move_accepted = False
+
+            # rejects invalid moves
+            while not move_accepted:
+
+                direction = random.choice(["up", "down", "right", "left"])
+
+                # positions after choice
+                dx, dy = x[i], y[i]
+
+                if direction == "up":
+                    dy += 1
+                elif direction == "down":
+                    dy -= 1
+                elif direction == "right":
+                    dx += 1
+                elif direction == "left":
+                    dx -= 1
+
+                # check bounds
+                if 0 <= dx < grid_size_x and 0 <= dy < grid_size_y:
+
+                    # accept move
+                    x[i] = dx
+                    y[i] = dy
+
+                    move_accepted = True
+
+            heat[y[i], x[i]] += 1
+
+
+        # store trajectories for selected particles
+        for j in range(n_trails):
+
+            traj_x[j].append(x[j])
+            traj_y[j].append(y[j])
+
+    return heat, traj_x, traj_y
+
+
