a66715d96ef972976502505ba37efcdcc387624e
Amanda Bowen <ab4798469@gmail.com>
2026-05-08 04:27:04 +0000

    edit

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from graphs import plot_results
 # set seed to make code reproducable
 random.seed(42)
 
-def random_walk(grid_size, N, steps, n_trails):
+def random_walk(grid_size_x = 101, grid_size_y = 101, N = 200, steps = 1000, n_trails = 5):
     # all particles start center
     x = [grid_size_x // 2] * N
     y = [grid_size_y // 2] * N
@@ -16,8 +16,8 @@ def random_walk(grid_size, N, steps, n_trails):
     heat = np.zeros((grid_size_y, grid_size_x))
 
     # store trajectories for a few particles
-    traj_x = [[center_x] for _ in range(n_trails)]
-    traj_y = [[center_y] for _ in range(n_trails)]
+    traj_x = [[x/N] for _ in range(n_trails)]
+    traj_y = [[y/N] for _ in range(n_trails)]
 
     # particle movement
     for step in range(steps):
@@ -62,12 +62,12 @@ def random_walk(grid_size, N, steps, n_trails):
             traj_x[j].append(x[j])
             traj_y[j].append(y[j])
 
-    return heat, traj_x, traj_y, grid_size
+    return heat, traj_x, traj_y, grid_size_x, grid_size_y
 
 
 if __name__ == "__main__":
 
-    heat, traj_x, traj_y, grid_size = random_walk_simulation()
+    heat, traj_x, traj_y, grid_size_x, grid_size_y = random_walk()
 
     plot_results(heat, traj_x, traj_y, grid_size)
 
